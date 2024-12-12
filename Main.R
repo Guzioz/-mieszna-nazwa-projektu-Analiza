836e3acbdafc50e6118440da6dd9f68567b36750
Guzio <s186843@student.pg.edu.pl>
2024-12-12 22:47:37 +0100

    nazwa czynniki

--- Main.R
+++ Main.R
@@ -1,3 +1,3 @@
 library(RCurl)
 x <- getURL('https://raw.githubusercontent.com/Guzioz/Smieszna-nazwa-projektu-Analiza/refs/heads/main/czynniki.csv')
-y <- read.csv(text = x)
+czynniki <- read.csv(text = x)
