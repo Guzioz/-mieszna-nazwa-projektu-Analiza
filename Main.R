e4388e1f480b88a42cfe5d2bce53034676549012
B33N197 <yankes197@gmail.com>
2024-12-12 23:07:45 +0100

    Cleaning

--- Main.R
+++ Main.R
@@ -3,3 +3,9 @@ x <- getURL('https://raw.githubusercontent.com/Guzioz/Smieszna-nazwa-projektu-An
 czynniki <- read.csv(text = x)
 summary(czynniki)
 str(czynniki)
+LICZBA_NA <- data.frame(liczba_brakow = colSums(is.na(czynniki)))
+LICZBA_NA
+WARTOSCI_BEZ_NA <- data.frame(liczba_unikatowych_wartosci = sapply(czynniki, function(x) length(unique(na.omit(x)))))
+WARTOSCI_BEZ_NA
+typ_danych <- data.frame(typ = sapply(czynniki, class))
+typ_danych
