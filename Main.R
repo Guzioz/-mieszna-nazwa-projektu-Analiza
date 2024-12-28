81f603665c4f0bb7d33e2e612b0f2ad985330b7b
Guzio <s186843@student.pg.edu.pl>
2024-12-28 14:00:29 +0100

    nazwa kat

--- Main.R
+++ Main.R
@@ -26,6 +26,8 @@ unique_values_all <- lapply(czynniki2[columns_to_check], unique)
 
 # Wyświetlenie liczby unikalnych wartości dla każdej kolumny
 print(unique_values_all)
+
+#Mapa brakujących wartości
 install.packages("naniar")
 library(naniar)
 vis_miss(czynniki2)
